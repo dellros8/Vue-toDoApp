95d0f95dd1d04c157dcc909d138d2f4c62436df4
unknown <dellros8@live.se>
2020-02-11 17:59:30 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="To-do-app"/>
+    <ToDo/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ToDo from './components/ToDo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ToDo
   }
 }
 </script>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h4>hej</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDo',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: blue;
+}
+</style>
